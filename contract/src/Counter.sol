a34f9ee2f8baf0b11ca9a1a1629b8ed383707525
Aroso Emmanuel <emmanueladedejiaroso@gmail.com>
2025-02-09 15:52:01 +0100

    wrote basic contract & applied openzeppelin

--- contract/src/Counter.sol
+++ contract/src/Counter.sol
@@ -1,14 +1,47 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
-contract Counter {
-    uint256 public number;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract NeuraLecto is ERC721URIStorage, Ownable {
+    uint256 private _tokenIds;
+    mapping(string => bool) private bookExists;
+    mapping(uint256 => string) public tokenIdToBook;
+    mapping(string => uint256) public bookPrices;
+    mapping(string => uint256) public bookVotes;
+    
+    event BookPurchased(address indexed buyer, uint256 tokenId, string bookTitle, uint256 price);
+    event BookVoted(string bookTitle, uint256 votes);
+
+    constructor() ERC721("NeuraLecto", "NLT") Ownable(msg.sender) {
+        _tokenIds = 0;
+
     }
 
-    function increment() public {
-        number++;
+    function setBookPrice(string memory _title, uint256 _price) public onlyOwner {
+        bookPrices[_title] = _price;
+    }
+
+    function purchaseBook(string memory _title, string memory _tokenURI) public payable returns (uint256) {
+        require(bookPrices[_title] > 0, "Book not available for sale");
+        require(msg.value >= bookPrices[_title], "Insufficient payment");
+        require(!bookExists[_title], "Book already purchased");
+        
+        _tokenIds++;
+        uint256 newItemId = _tokenIds;
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
+        
+        tokenIdToBook[newItemId] = _title;
+        bookExists[_title] = true;
+        
+        emit BookPurchased(msg.sender, newItemId, _title, msg.value);
+        return newItemId;
+    }
+
+    function voteBook(string memory _title) public {
+        bookVotes[_title]++;
+        emit BookVoted(_title, bookVotes[_title]);
     }
 }
